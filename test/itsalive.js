e3c0178c153bee6df85b914a9b39299eabfb66ab
derrickbeining <derrick.beining@gmail.com>
2017-08-08 15:48:12 -0400

    kicking ass, switchin drivers

--- test/itsalive.js
+++ test/itsalive.js
@@ -1,18 +1,30 @@
-var expect = require('chai').expect;
+var chai = require('chai');
+var expect = chai.expect;
+const spies = require('chai-spies');
+chai.use(spies);
 
-describe('Testing suite capabilities', function () {
-  it('confirms basic arithmetic', function () {
-    expect(2+2).to.equal(4);
-  });
-});
+// describe('Testing suite capabilities', function () {
+//   it('confirms basic arithmetic', function () {
+//     expect(2+2).to.equal(4);
+//   });
+// });
 
-describe('Testing asynchronous code', function () {
-  it('it takes one second', function (done) {
-      const firstDate = Date.now();
-      setTimeout(function () {
-        const timeTaken = Date.now() - firstDate;
-        expect(timeTaken).to.be.closeTo(1000, 50);
-        done();
-      }, 1000)
-  })
-})
+// describe('Testing asynchronous code', function () {
+//   it('it takes one second', function (done) {
+//       const firstDate = Date.now();
+//       setTimeout(function () {
+//         const timeTaken = Date.now() - firstDate;
+//         expect(timeTaken).to.be.closeTo(1000, 50);
+//         done();
+//       }, 1000)
+//   })
+// })
+
+// describe('For each', function () {
+//   const arr = [3, 6, 7, 8];
+//   logger = chai.spy((el) => { console.log(el) });
+//   it('invokes function for every element', function () {
+//     arr.forEach(logger);
+//     expect(logger).to.have.been.called.exactly(4);
+//   })
+// })
